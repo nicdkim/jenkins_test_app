af14a81650bafd407c144659ae1845dba972836e
nicdkim <zzsehdrb@naver.com>
2024-12-05 16:45:27 +0900

    webconfig

---
+++ src/main/java/com/ohgiraffers/jenkins_test_app/menu/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.jenkins_test_app.menu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://cloud-test-five-virid.vercel.app") // 클라이언트 URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
